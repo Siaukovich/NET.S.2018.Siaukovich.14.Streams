efff9a49877450a147f3b490b4f6abf43e51ca6c
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-07-20 22:53:12 +0300

    Console app and App.Config were updated.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using static StreamsDemo.StreamsExtension;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = ConfigurationManager.AppSettings["sourceFilePath"];
+
+            var destination = ConfigurationManager.AppSettings["destinationFilePath"];
+
+            Console.WriteLine($"ByteCopy() done. Total bytes: {ByByteCopy(source, destination)}");
+            Console.WriteLine($"BlockCopy() done. Total bytes: {ByBlockCopy(source, destination)}");
+            Console.WriteLine($"LineCopy() done. Total lines: {ByLineCopy(source, destination)}");
+            Console.WriteLine($"ContentEquals() done. Files equal: {IsContentEquals(source, destination)}");
+
+
+            //Console.WriteLine($"InMemoryByteCopy() done. Total bytes: {InMemoryByByteCopy(source, destination)}");
+
+            //Console.WriteLine(IsContentEquals(source, destination));
+
+            //etc
+
+            Console.ReadKey();
+        }
+    }
+}
